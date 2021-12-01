2e295ded2271a165de0ef406142fc7a2d1d94063
Oliver Godby <maleghast@gmail.com>
2021-12-01 17:51:42 +0000

    First commit for Day2 - doing the prep work

---
+++ Day2.main/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to Day2.main
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ Day2.main/test/Day2/main_test.clj
@@ -0,0 +1,7 @@
+(ns Day2.main-test
+  (:require [clojure.test :refer :all]
+            [Day2.main :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
