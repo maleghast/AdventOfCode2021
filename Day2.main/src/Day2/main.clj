351f32541873f0131ff781cd7bb4bf1bedc3a999
Oliver Godby <maleghast@gmail.com>
2021-12-02 11:33:43 +0000

    Puzzle #2 completed

--- Day2.main/src/Day2/main.clj
+++ Day2.main/src/Day2/main.clj
@@ -1,12 +1,68 @@
 (ns Day2.main
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str])
   (:gen-class))
 
-(defn greet
-  "Callable entry point to the application."
+(def horizontal-pos (atom 0))
+(def depth-pos (atom 0))
+(def aim (atom 0))
+
+(defn import-instructions-data
+  []
+  (into
+   []
+   (map
+    #(str/split (first %) #" ")
+    (partition
+     1
+     (str/split
+      (slurp (io/resource "input.txt"))
+      #"\n")))))
+
+(defn prepare-instructions
   [data]
-  (println (str "Hello, " (or (:name data) "World") "!")))
+  (into
+   []
+   (map
+    (fn
+      [vec]
+      (update
+       vec
+       1
+       #(Integer/parseInt %)))
+    data)))
+
+(defn simple-nav
+  []
+  (doseq [instruction (prepare-instructions (import-instructions-data))]
+    (cond
+      (= "forward" (first instruction)) (swap! horizontal-pos + (last instruction))
+      (= "up" (first instruction)) (swap! depth-pos - (last instruction))
+      (= "down" (first instruction)) (swap! depth-pos + (last instruction)))))
+
+(defn complex-nav
+  []
+  (doseq [instruction (prepare-instructions (import-instructions-data))]
+    (cond
+      (= "forward" (first instruction)) (do
+                                          (swap! horizontal-pos + (last instruction))
+                                          (swap! depth-pos + (* (last instruction) @aim)))
+      (= "up" (first instruction)) (swap! aim - (last instruction))
+      (= "down" (first instruction)) (swap! aim + (last instruction)))))
+
+(defn zero-atoms
+  []
+  (do
+    (reset! horizontal-pos 0)
+    (reset! depth-pos 0)
+    (reset! aim 0)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (greet {:name (first args)}))
+  []
+  (zero-atoms)
+  (simple-nav)
+  (println "Simple Nav Result -> " (* @horizontal-pos @depth-pos))
+  (zero-atoms)
+  (complex-nav)
+  (println "Complex Nav Result -> " (* @horizontal-pos @depth-pos)))
