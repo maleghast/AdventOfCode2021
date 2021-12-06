281d5c9c20ee497383925b80a4e78f277d57313e
Oliver Godby <maleghast@gmail.com>
2021-12-06 09:59:12 +0000

    Getting Day 6 ready - first commit

---
+++ Day6.main/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to Day6.main
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ Day6.main/test/Day6/main_test.clj
@@ -0,0 +1,7 @@
+(ns Day6.main-test
+  (:require [clojure.test :refer :all]
+            [Day6.main :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
